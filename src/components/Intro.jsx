e5d69ea48c67bb93e27b802536180c8d6a824842
Katie Toney <kathryn.toney@gmail.com>
2023-06-19 10:51:01 -0400

    init project

---
+++ src/components/Intro.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+export default function Intro() {
+    const [clientInfo, setClientInfo] = useState(
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        language: '',
+        pronouns: '',
+        )
+}
